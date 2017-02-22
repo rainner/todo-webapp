beddf4495acb92f3de3489ea03cf2bccf585e660
Rainner Lins <rainnerlins@gmail.com>
2017-02-22 05:02:24 -0500

    Changes to webpack config file

--- webpack.config.js
+++ webpack.config.js
@@ -4,76 +4,78 @@ var webpack = require( "webpack" );
 
 module.exports = {
 
-  entry: "./src/main.js",
+    entry: "./src/main.js",
 
-  output: {
-    path: path.resolve( __dirname, "./public/js" ),
-    publicPath: "/public/js/",
-    filename: "bundle.js"
-  },
+    output: {
+        path: path.resolve( __dirname, "./public/js" ),
+        publicPath: "/public/js/",
+        filename: "bundle.js"
+    },
 
-  module: {
-    rules: [
-
-      {
-        test: /\.vue$/,
-        loader: "vue-loader",
-        options: {
-          loaders: {
-            "scss": "vue-style-loader!css-loader!sass-loader",
-            "sass": "vue-style-loader!css-loader!sass-loader?indentedSyntax"
-          }
+    resolve: {
+        alias: {
+            "vue$": "vue/dist/vue.common.js",
         }
-      },
+    },
 
-      {
-        test: /\.js$/,
-        loader: "babel-loader",
-        exclude: /node_modules/
-      },
+    devServer: {
+        host: "192.168.0.102",
+        port: 8888,
+        clientLogLevel: "info",
+        historyApiFallback: true,
+        noInfo: true,
+        inline: true,
+        compress: false,
+    },
 
-    ]
-  },
+    performance: {
+        hints: false,
+    },
 
-  resolve: {
-    alias: {
-      "vue$": "vue/dist/vue.common.js"
-    }
-  },
+    module: {
+        rules: [
+            {
+                test: /\.vue$/,
+                loader: "vue-loader",
+                options: {
+                    loaders: {
+                        "scss": "vue-style-loader!css-loader!postcss-loader!sass-loader",
+                        "sass": "vue-style-loader!css-loader!postcss-loader!sass-loader?indentedSyntax",
+                    }
+                }
+            },
+            {
+                test: /\.js$/,
+                loader: "babel-loader",
+                exclude: /node_modules/,
+            },
+        ]
+    },
 
-  devServer: {
-    host: "192.168.0.102",
-    port: 8888,
-    historyApiFallback: true,
-    noInfo: true,
-  },
-
-  performance: {
-    hints: false
-  },
-
-  devtool: "#eval-source-map",
+    devtool: "#eval-source-map",
 };
 
 /**
  * Production build
  */
-if (process.env.NODE_ENV === "production") {
-  module.exports.devtool = "#source-map"
-  module.exports.plugins = (module.exports.plugins || []).concat([
-    new webpack.DefinePlugin({
-      "process.env": {
-        NODE_ENV: '"production"'
-      }
-    }),
-    new webpack.optimize.UglifyJsPlugin({
-      sourceMap: true,
-      compress: {
-        warnings: false
-      }
-    }),
-    new webpack.LoaderOptionsPlugin({
-      minimize: true
-    })
-  ])
+if( process.env.NODE_ENV === "production" )
+{
+    module.exports.devtool = "#source-map";
+
+    module.exports.plugins = ( module.exports.plugins || [] ).concat([
+        new webpack.DefinePlugin({
+            "process.env": {
+                NODE_ENV: '"production"',
+            }
+        }),
+        new webpack.optimize.UglifyJsPlugin({
+            sourceMap: true,
+            compress: {
+                warnings: false,
+            }
+        }),
+        new webpack.LoaderOptionsPlugin({
+            minimize: true,
+        }),
+    ]);
 }
