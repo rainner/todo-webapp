b8d42445841a4a5ba63ad7da9232b74d1b249acc
Rainner Lins <rainnerlins@gmail.com>
2017-03-15 21:13:46 -0400

    Moved some custom vue directives to main.js

--- src/main.js
+++ src/main.js
@@ -2,9 +2,21 @@
  * App entry file
  */
 import {} from "./scripts/Polyfill";
-
 import Vue from "vue";
 import App from "./components/App.vue";
+import Dropdown from "./scripts/Dropdown";
+import Tooltip from "./scripts/Tooltip";
+
+var tooltip = new Tooltip();
+
+Vue.directive( "tooltip", {
+    bind: function( el ) { tooltip.select( el ); },
+    unbind: function( el ) { tooltip.unselect( el ); },
+});
+
+Vue.directive( "dropdown", {
+    bind: function( el ) { new Dropdown( el ); },
+});
 
 new Vue({
   el: "#app",
