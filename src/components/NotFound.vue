737865b4e73c6ca86f16bd30a068a078850b410f
Rainner Lins <rainnerlins@gmail.com>
2017-03-15 21:18:34 -0400

    Changes and fixes

--- src/components/NotFound.vue
+++ src/components/NotFound.vue
@@ -1,17 +1,19 @@
 <template>
     <div class="todo-info-page">
+
         <h4>Page Not Found</h4>
-        <p>
+
+        <div class="push-bottom">
             Oops, looks like nothing was found for the current URL route specified.
-            Maybe whatever was here before got deleted, or the URL route might be incorrect.
-            Double-check and try again, or click around to go somewhere else. &nbsp;
-            <span class="icon-link text-danger" v-html="curUrl()"></span>
-        </p>
+            Maybe the thing that was here before got deleted, or the URL might be incorrect.
+            Double-check and try again, or click around to go somewhere else.
+            <span class="icon-link icon-pr text-danger" v-html="getUrl()"></span>
+        </div>
+
         <hr />
-        <p>
-            <button class="btn bg-bright-hover icon-home icon-pr shadow-paper" @click="goHome()">App home</button>
-            <button class="btn bg-bright-hover icon-reload icon-pr shadow-paper" @click="tryAgain()">Try again</button>
-        </p>
+        <a class="btn bg-bright-hover icon-home icon-pr shadow-paper" href="#/home">App home</a>
+        <a class="btn bg-bright-hover icon-reload icon-pr shadow-paper" href="#" @click.prevent="reload()">Try again</a>
+
     </div>
 </template>
 
@@ -20,35 +22,31 @@
 // component
 export default {
 
-    // component name
-    name: "notfound",
-
-    // component data
-    data() {
-        return {
-
-        }
+    // component props
+    props: {
+        options: { type: Object, default: {}, required: false },
+        user: { type: Object, default: {}, required: false },
     },
 
     // component methods
     methods: {
 
+        // for passing method calls to parent
+        emit: function()
+        {
+            return this.$parent.emit.apply( this.$parent, arguments );
+        },
+
         // get current address
-        curUrl: function()
+        getUrl: function()
         {
             return location.href || "";
         },
 
         // reload the page
-        tryAgain: function()
+        reload: function()
         {
             location.reload();
-        },
-
-        // go to home view
-        goHome: function()
-        {
-            this.$parent.unselectTodos();
         },
 
     }
