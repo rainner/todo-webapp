c9f7aa13104a70f24ef311aed8bbbc261fd1cc3e
Rainner Lins <rainnerlins@gmail.com>
2017-03-26 17:16:15 -0400

    Custom dropdowns now auto-position to account for screen edge overflow.

--- src/scripts/Dropdown.js
+++ src/scripts/Dropdown.js
@@ -1,7 +1,5 @@
 /**
  * Dropdown helper class.
- * Helps CSS dropdowns work better by
- * binding click events to elements.
  *
  * @author     Rainner Lins <http://rainnerlins.com/>
  * @copyright  (c) All Rights Reserved
@@ -19,18 +17,20 @@
     var Factory = function( dropdown, options )
     {
         this._options = Object.assign( {
-            // optional timer to auto hide the dropdown
-            autoHideTimeout: 0,
             // ...
         }, options );
 
-        this._dropdown = null;
-        this._trigger  = null;
-        this._timeout  = null;
-        this._visible  = null;
-        this._showMenu = this._showMenu.bind( this );
-        this._hideMenu = this._hideMenu.bind( this );
+        this._visible    = false;
+        this._dropdown   = null;
+        this._trigger    = null;
+        this._list       = null;
+        this._listClass  = "";
+        this._showMenu   = this._showMenu.bind( this );
+        this._hideMenu   = this._hideMenu.bind( this );
+        this._toggleMenu = this._toggleMenu.bind( this );
+
         this.select( dropdown );
+        // window.addEventListener( "click", this._hideMenu );
     };
 
     // class prototype
@@ -44,34 +44,87 @@
 
             this._dropdown = ( dropdown instanceof Element ) ? dropdown : null;
             this._trigger  = this._dropdown ? this._dropdown.querySelector( ".dropdown-trigger" ) : null;
+            this._list     = this._dropdown ? this._dropdown.querySelector( "ul" ) : null;
 
-            if( this._dropdown && this._trigger && !this._dropdown.hasAttribute( "data-dropdown" ) )
+            if( this._dropdown && this._trigger && this._list && !this._dropdown.hasAttribute( "data-dropdown" ) )
             {
+                this._listClass = this._list.className;
                 this._dropdown.setAttribute( "data-dropdown", 1 );
                 this._dropdown.addEventListener( "mouseup", this._hideMenu );
                 this._dropdown.addEventListener( "mouseleave", this._hideMenu );
-                this._trigger.addEventListener( "click", this._showMenu );
+                this._trigger.addEventListener( "click", this._toggleMenu );
             }
         },
 
-        // handle dropdown trigger event
+        // modify list class to account for going off the screen
+        _adjustList: function()
+        {
+            if( this._list )
+            {
+                var box        = this._list.getBoundingClientRect(),
+                    boxWidth   = this._list.offsetWidth,
+                    boxHeight  = this._list.offsetHeight,
+                    pageLeft   = 0,
+                    pageTop    = 0,
+                    pageRight  = window.innerWidth,
+                    pageBottom = window.innerHeight;
+
+                if( box.top < pageTop ) // move down
+                {
+                    this._list.classList.remove( "dropdown-top" );
+                    this._list.classList.add( "dropdown-bottom" );
+                }
+                if( ( box.top + boxHeight ) > pageBottom ) // move up
+                {
+                    this._list.classList.remove( "dropdown-bottom" );
+                    this._list.classList.add( "dropdown-top" );
+                }
+                if( box.left < pageLeft ) // move right
+                {
+                    this._list.classList.remove( "dropdown-left" );
+                    this._list.classList.add( "dropdown-right" );
+                }
+                if( ( box.left + boxWidth ) > pageRight ) // move left
+                {
+                    this._list.classList.remove( "dropdown-right" );
+                    this._list.classList.add( "dropdown-left" );
+                }
+            }
+        },
+
+        // set dropdown to active
         _showMenu: function( e )
         {
+            e.preventDefault();
+            e.stopPropagation();
+
             if( this._dropdown && !this._visible )
             {
                 this._visible = true;
                 this._dropdown.classList.add( "active" );
+                setTimeout( this._adjustList.bind( this ), 100 );
             }
         },
 
-        // handle dropdown hide event
+        // set dropdown to inactive (hide)
         _hideMenu: function( e )
         {
+            e.preventDefault();
+            e.stopPropagation();
+
             if( this._dropdown && this._visible )
             {
                 this._visible = false;
                 this._dropdown.classList.remove( "active" );
+                this._list.className = this._listClass;
             }
+        },
+
+        // toggle dropdown state
+        _toggleMenu: function( e )
+        {
+            if( this._visible ) { this._hideMenu( e ); }
+            else { this._showMenu( e ); }
         },
     };
 
